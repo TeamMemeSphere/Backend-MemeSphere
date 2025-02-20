04b0e92271f47129fc8e75e69efb73dddf9282ae
SeoyeonPark1223 <trispark1223@gmail.com>
2025-02-20 18:22:55 +0900

    #143 Fix: 밈코인 랭크 10분마다 업데이트 추가

--- src/main/java/com/memesphere/domain/chartdata/scheduler/ChartDataScheduler.java
+++ src/main/java/com/memesphere/domain/chartdata/scheduler/ChartDataScheduler.java
@@ -3,6 +3,8 @@ package com.memesphere.domain.chartdata.scheduler;
 import com.memesphere.domain.binance.dto.response.BinanceTickerResponse;
 import com.memesphere.domain.binance.service.BinanceQueryService;
 import com.memesphere.domain.chartdata.entity.ChartData;
+import com.memesphere.domain.chartdata.repository.ChartDataRepository;
+import com.memesphere.domain.dashboard.dto.response.DashboardTrendResponse;
 import com.memesphere.domain.notification.service.PushNotificationService;
 import com.memesphere.global.apipayload.code.status.ErrorStatus;
 import com.memesphere.global.apipayload.exception.GeneralException;
@@ -15,6 +17,7 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
@@ -28,6 +31,7 @@ public class ChartDataScheduler {
     private final MemeCoinQueryService memeCoinQueryService;
     private final LoggedInUserStore loggedInUserStore;
     private final PushNotificationService pushNotificationService;
+    private final ChartDataRepository chartDataRepository;
 
     @Scheduled(cron = "0 0/10 * * * ?") // 0, 10, 20, 30, 40, 50분에 실행
     @Transactional
@@ -36,6 +40,9 @@ public class ChartDataScheduler {
         Set<Long> loggedInUsers = loggedInUserStore.getLoggedInUsers();
 
         List<MemeCoin> memeCoins = memeCoinRepository.findAll();
+
+        // 이전에 기록된 top5 밈코인
+        List<MemeCoin> prevCoinList = memeCoinRepository.findTop5OrderByRank();
 
         for (MemeCoin memeCoin : memeCoins) {
             try {
@@ -53,6 +60,30 @@ public class ChartDataScheduler {
 
         for (Long userId : loggedInUsers) {
             pushNotificationService.send(userId);
+        }
+
+        // 밈코인 랭킹 업데이트
+        updateRank(prevCoinList);
+    }
+
+    public void updateRank(List<MemeCoin> prevCoinList) {
+        // 최신 거래량 top5 밈코인-차트데이터
+        List<ChartData> updatedChartDataList = chartDataRepository.findTop5OrderByVolumeDesc();
+
+        if (prevCoinList != null) {
+            // 현재 top5에 들지 않는 밈코인 순위 처리
+            List<MemeCoin> currCoinList = updatedChartDataList.stream().map(ChartData::getMemeCoin).toList();
+            for (MemeCoin prevCoin : prevCoinList) {
+                if (!currCoinList.contains(prevCoin)) prevCoin.updateRank(null);
+            }
+        }
+
+        // 현재 top5 밈코인
+        for (int i = 0; i < 5; i++) {
+            MemeCoin memeCoin = updatedChartDataList.get(i).getMemeCoin();
+
+            Integer currRank = i + 1;
+            memeCoin.updateRank(currRank);
         }
     }
 }
